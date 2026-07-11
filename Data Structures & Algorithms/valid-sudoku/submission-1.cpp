0a9d551b4e804d02a01d5da34d3766a391bdc4ad
Aryan Verma <aryanverma7109@gmail.com>
2026-07-11 10:01:35 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> rows[9];
+        unordered_set<char> cols[9];
+        unordered_set<char> squares[9];
+
+        for(int row = 0; row < 9; row++){
+            for(int col = 0; col < 9; col++){
+                int num = board[row][col];
+
+                if(num == '.')continue;
+                int sqId = (row / 3) * 3 + (col / 3);
+                if(rows[row].count(num) || cols[col].count(num) || squares[sqId].count(num)){
+                    return false;
+                }
+                rows[row].insert(num);
+                cols[col].insert(num);
+                squares[sqId].insert(num);
+            }
+        }
+        return true;
+    }
+};
